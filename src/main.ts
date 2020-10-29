9b0d3d94644b2512bced2d2fd8c93be0a6910a0b
Tomasz Trebacz <73560937+TomaszTrebacz@users.noreply.github.com>
2020-10-29 13:06:37 +0100

    Create Blog-Service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
+  Logger.log(
+    `User-service listening on http://localhost:${process.env.APP_PORT}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
