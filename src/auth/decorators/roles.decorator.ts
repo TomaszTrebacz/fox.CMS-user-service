319537af48f0d50f879e9a7c4832d27154fd9d9d
Tomasz Trębacz <trebacztomasz@outlook.com>
2020-12-10 01:37:09 +0100

    Refactor code relative to JWT [user-service][blog-service]
    - remove info about role and keep only id in access token,
    - refactor guards & jwt strategy

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,3 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
+import { userRole } from 'src/users/enums/userRole.enum';
 
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
+export const Roles = (...roles: userRole[]) => SetMetadata('roles', roles);
--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -2,31 +2,40 @@ import {
   Injectable,
   ExecutionContext,
   UnauthorizedException,
+  Inject,
 } from '@nestjs/common';
 import { GqlAuthGuard } from './gql-auth.guard';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { userRole } from 'src/users/enums/userRole.enum';
+import { AuthenticationError } from 'apollo-server-core';
 
 @Injectable()
 export class RolesGuard extends GqlAuthGuard {
-  constructor(private reflector: Reflector) {
+  constructor(
+    @Inject('RedisDbService') private readonly redisService,
+    private reflector: Reflector,
+  ) {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<userRole[]>('roles', context.getHandler());
 
     if (roles) {
       const ctx = GqlExecutionContext.create(context);
       const request = ctx.getContext().req;
-      const role = <userRole>request.user.role;
 
-      if (!roles.includes(role)) {
-        throw new UnauthorizedException('Wrong role!');
+      const actualRole = await this.redisService.getValue(
+        request.user.id,
+        'role',
+      );
+
+      if (!roles.includes(actualRole)) {
+        throw new AuthenticationError('Wrong role!');
       }
     }
 
-    return super.canActivate(context);
+    return true;
   }
 }
