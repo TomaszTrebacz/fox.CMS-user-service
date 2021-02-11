2da65b11a16303a2159ff11f8eb86c164043a933
Tomasz Trębacz <trebacztomasz@outlook.com>
2021-02-11 20:10:01 +0100

    feat(apollo-federation): add resolveReference resolver

---
+++ src/users/resolvers/resolveReference/resolveReference.resolver.ts
@@ -0,0 +1,12 @@
+import { Resolver, ResolveReference } from '@nestjs/graphql';
+import { UsersService } from 'src/users/service/users.service';
+
+@Resolver('User')
+export class resolveReferenceResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @ResolveReference()
+  async resolveReference(reference: { typename: string; id: string }) {
+    return await this.usersService.findOneById(reference.id);
+  }
+}
