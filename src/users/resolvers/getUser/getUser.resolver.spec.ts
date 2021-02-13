9d9b5cd51f5c2086f08c07c1f8587b3a761005cc
Tomasz Trębacz <trebacztomasz@outlook.com>
2021-02-13 23:27:47 +0100

    feat: add tests for getUser resolver

---
+++ src/users/resolvers/getUser/getUser.resolver.spec.ts
@@ -0,0 +1,61 @@
+import { Test } from '@nestjs/testing';
+import { UsersService } from '../../../users/service/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../../../database/entities/user.entity';
+import { getUserResolver } from './getUser.resolver';
+import { fakeUsers } from '../../../database/seeds/data/fakeUsers.data';
+import { regexPHONE } from '../../../utils';
+
+describe('getUserResolver', () => {
+  let resolver: getUserResolver;
+  let usersService: UsersService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({ keepConnectionAlive: true }),
+        TypeOrmModule.forFeature([UserEntity]),
+      ],
+      providers: [getUserResolver, UsersService],
+    }).compile();
+
+    resolver = module.get<getUserResolver>(getUserResolver);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(getUserResolver).toBeDefined();
+  });
+
+  describe('if user exists', () => {
+    it('should return the user', async () => {
+      const user = await resolver.getUser(fakeUsers[5].id);
+
+      expect(user).toEqual(
+        expect.objectContaining({
+          id: fakeUsers[5].id,
+          firstName: fakeUsers[5].firstName,
+          lastName: fakeUsers[5].lastName,
+          password: expect.any(String),
+          phoneNumber: expect.stringMatching(regexPHONE),
+          created: expect.any(Date),
+          updated: expect.any(Date),
+        }),
+      );
+    });
+  });
+
+  describe('otherwise', () => {
+    it('should return the error', async () => {
+      const FakeNotExistID = '948091ae-9cb7-4770-90ab-f058f66ee5c7';
+
+      try {
+        await resolver.getUser(FakeNotExistID);
+      } catch (err) {
+        expect(err.message).toEqual(
+          `Can not find user with id: ${FakeNotExistID}`,
+        );
+      }
+    });
+  });
+});
